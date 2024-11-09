d13e232faf8d04454bfcf878b1a12a50cd52976a
SohalSawardekar <sohalsawardekar11@gmail.com>
2024-11-09 12:57:56 +0530

    bug fix

--- lib/constants/imports.dart
+++ lib/constants/imports.dart
@@ -1,5 +1,5 @@
 export 'package:flutter/material.dart';
-export 'package:sentrix/test.dart';
+export 'package:sentrix/screens/home.dart';
 export 'package:firebase_core/firebase_core.dart';
 export '/firebase_options.dart';
 export 'package:firebase_auth/firebase_auth.dart';
