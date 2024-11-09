8e41002fe9fd8d089b97881715c2c34ce0e93a01
SohalSawardekar <sohalsawardekar11@gmail.com>
2024-11-09 11:47:22 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:sentrix/test.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'constants/imports.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
+      home: LoginPage(),
     );
   }
 }
